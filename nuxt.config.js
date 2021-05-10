c52aa37c114284685727775bb3c6c6fb157f3b8d
Phillip Linley <philliplinley@Phillips-MacBook-Pro.local>
2021-05-09 21:04:35 -0400

    updated name

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'unsanctioned-pro',
+    title: '[Unsanctioned Pro]',
     htmlAttrs: {
       lang: 'en'
     },
